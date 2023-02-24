2e4e31128af5e65214a75a7eab4476bf5df29cc0
GeorgeSalu <george.salu10@gmail.com>
2023-02-24 11:27:18 -0300

    correção classe AlgafoodApiApplicationTests

--- algafood-api/src/test/java/com/algawors/algafood/AlgafoodApiApplicationTests.java
+++ algafood-api/src/test/java/com/algawors/algafood/AlgafoodApiApplicationTests.java
@@ -1,13 +1,16 @@
 package com.algawors.algafood;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AlgafoodApiApplicationTests {
+public class AlgafoodApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
