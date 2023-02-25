efbcdc7d317d023ef8ee9b45c8c083123dde88b2
GeorgeSalu <george.salu10@gmail.com>
2023-02-25 10:02:01 -0300

    remove contrutor não utilizado

--- algafood-api/src/main/java/com/algawors/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algawors/algafood/di/notificacao/NotificadorEmail.java
@@ -7,8 +7,6 @@ import com.algawors.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	public NotificadorEmail() {}
-	
 	@Override
 	public void notificar(Cliente cliente,String mensagem) {
 		System.out.printf("Notificando %s atraves do email %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
