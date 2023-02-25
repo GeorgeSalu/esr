0507ae8c6b91d37c4ff1d02aeeaed7f82eecf54f
GeorgeSalu <george.salu10@gmail.com>
2023-02-25 20:08:35 -0300

    criando metodos de callback do ciclo de vida dos beans

--- algafood-api/src/main/java/com/algawors/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algawors/algafood/di/service/AtivacaoClienteService.java
@@ -1,5 +1,8 @@
 package com.algawors.algafood.di.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +17,16 @@ public class AtivacaoClienteService {
 	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
 	private Notificador notificador;
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("INIT");
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("DESTROY");
+	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
