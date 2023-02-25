5d20db8bc4e85d44f5a6b48a5a9c1432137c7ac2
GeorgeSalu <george.salu10@gmail.com>
2023-02-25 19:56:25 -0300

    remove estrutura pra explicação de profiles

--- algafood-api/src/main/java/com/algawors/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algawors/algafood/di/notificacao/NotificadorEmail.java
@@ -1,18 +1,12 @@
 package com.algawors.algafood.di.notificacao;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.algawors.algafood.di.modelo.Cliente;
 
-@Profile("prod")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
-	
-	public NotificadorEmail() {
-		System.out.println("NotificadorEmail Real");
-	}
 	
 	@Override
 	public void notificar(Cliente cliente,String mensagem) {
