e8a513adf326ef3b7a3c0446a89ebcc893ed6b3a
GeorgeSalu <george.salu10@gmail.com>
2023-03-03 14:07:58 -0300

    quando statuscode 200 e desnecessario ainda marcar com statuscode ok

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,12 +3,10 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -32,7 +30,6 @@ public class CozinhaController {
 		return new CozinhaXmlWrapper(cozinhaRepository.listar());
 	}
 	
-	@ResponseStatus(code = HttpStatus.OK)
 	@GetMapping("/{cozinhaId}")
 	public Cozinha bucar(@PathVariable Long cozinhaId) {
 		return cozinhaRepository.buscar(cozinhaId);
