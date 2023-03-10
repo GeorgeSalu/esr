2834df4030739c9ec9ecfeecaed4f62092ab8088
GeorgeSalu <george.salu10@gmail.com>
2023-03-10 15:17:07 -0300

    criando consultas com query methods

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,9 +1,14 @@
 package com.algaworks.algafood.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
@@ -13,9 +18,9 @@ public class TesteController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-//	@GetMapping(value = "/cozinhas/por-nome")
-//	public List<Cozinha> cozinhasPorNome(@RequestParam String nome) {
-//		return cozinhaRepository.consultarPorNome(nome);
-//	}
+	@GetMapping(value = "/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(@RequestParam String nome) {
+		return cozinhaRepository.findByNome(nome);
+	}
 	
 }
