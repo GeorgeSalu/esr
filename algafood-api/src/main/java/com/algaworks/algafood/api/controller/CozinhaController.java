bc7ba47be7ce8580f1eace5079eda42b96f91592
GeorgeSalu <george.salu10@gmail.com>
2023-03-23 14:48:49 -0300

    estendendo ResponseStatusException

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -71,11 +71,7 @@ public class CozinhaController {
 	@DeleteMapping("/{cozinhaId}")
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long cozinhaId) {
-		try {			
-			cadastroCozinha.excluir(cozinhaId);
-		}catch (EntidadeNaoEncontradaException e) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.toString());
-		}
+		cadastroCozinha.excluir(cozinhaId);
 	}
 	
 }
