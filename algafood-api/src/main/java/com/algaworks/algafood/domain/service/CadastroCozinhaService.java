7037e1302e5ff923a51fe4af415d690b8b8897fe
GeorgeSalu <george.salu10@gmail.com>
2023-05-01 15:03:51 -0300

    desafio lançando exceptions de granularidade fina

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
@@ -5,8 +5,8 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
@@ -27,7 +27,7 @@ public class CadastroCozinhaService {
 		try {
 			cozinhaRepository.deleteById(cozinhaId);
 		}catch (EmptyResultDataAccessException e) {
-			throw new EntidadeNaoEncontradaException(String.format(MSG_COZINHA_NAO_ENCONTRADA,cozinhaId));
+			throw new CozinhaNaoEncontradaException(cozinhaId);
 		}catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(String.format(MSG_COZINHA_EM_USO, cozinhaId));
 		}
