fcce5e74e23cb7a0dead0240f2f708f356442810
GeorgeSalu <george.salu10@gmail.com>
2023-05-08 13:00:08 -0300

    formata arquivo ddl.sql

--- algafood-api/src/main/resources/ddl.sql
+++ algafood-api/src/main/resources/ddl.sql
@@ -82,8 +82,8 @@ create table restaurante (id bigint not null auto_increment, data_atualizacao da
 create table restaurante_forma_pagamento (restaurante_id bigint not null, forma_pagamento_id bigint not null) engine=InnoDB
 create table usuario (id bigint not null auto_increment, data_cadastro datetime not null, email varchar(255) not null, nome varchar(255) not null, senha varchar(255) not null, primary key (id)) engine=InnoDB
 create table usuario_grupo (usuario_id bigint not null, grupo_id bigint not null) engine=InnoDB
-alter table cidade add constraint FKkworrwk40xj58kevvh3evi500 foreign key (estado_id) references estado (id)
 
+alter table cidade add constraint FKkworrwk40xj58kevvh3evi500 foreign key (estado_id) references estado (id)
 alter table grupo_permissao add constraint FKh21kiw0y0hxg6birmdf2ef6vy foreign key (permissao_id) references permissao (id)
 alter table grupo_permissao add constraint FKta4si8vh3f4jo3bsslvkscc2m foreign key (grupo_id) references grupo (id)
 alter table produto add constraint FKb9jhjyghjcn25guim7q4pt8qx foreign key (restaurante_id) references restaurante (id)
