029a37d8cc53540fb43c67c2380bf790939b7310
GeorgeSalu <george.salu10@gmail.com>
2023-05-10 09:47:47 -0300

    corrige title em ProblemType

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -3,7 +3,7 @@ package com.algaworks.algafood.api.exceptionhandler;
 public enum ProblemType {
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
-	ERRO_NEGOCIO("/erro-negocio", "Violacao de regra de negocio");
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
 
 	private String title;
 	private String uri;
